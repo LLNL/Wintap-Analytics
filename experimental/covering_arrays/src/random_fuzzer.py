23a32e0e211e8aaa2131310ab9dd1d6d3843f237
Jason Bernstein <Bernstein8@llnl.gov>
2024-05-08 11:32:09 -0700

    Reformat argument parsing

--- experimental/covering_arrays/src/random_fuzzer.py
+++ experimental/covering_arrays/src/random_fuzzer.py
@@ -5,24 +5,12 @@ import utils
 
 def get_args():
     # Get command-line arguments
-    
-    parser = argparse.ArgumentParser(description="Covering Array Fuzzer")
 
-    parser.add_argument("-n",
-                        "--number",
-                        type=int,
-                        help="Number of times to fuzz utility")
-    
-    parser.add_argument("-u",
-                        "--utility",
-                        type=str,
-                        help="Name of the utility")
-    
-    parser.add_argument("-d",
-                        "--debug",
-                        action=argparse.BooleanOptionalAction,
-                        default=False,
-                         help="Include this flag to print debugging information")
+    parser = argparse.ArgumentParser(description="Covering Array Fuzzer")
+    parser.add_argument("-n", "--number", type=int, help="Number of times to fuzz utility")
+    parser.add_argument("-u", "--utility", type=str,  help="Name of the utility")
+    parser.add_argument("-d", "--debug", action=argparse.BooleanOptionalAction,
+                        default=False, help="Include this flag to print debugging information")
 
     return parser.parse_args()
 
