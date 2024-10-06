99580df0e3813f08120fe3a958afa01d50888cc0
Grant Johnson <johnson30@llnl.gov>
2024-10-06 11:35:06 -0700

    WIP - added semi-colons so it can run as a script

--- annotations/acme4/sql/Actor1.sql
+++ annotations/acme4/sql/Actor1.sql
@@ -19,7 +19,7 @@ select local_ip_addr, remote_ip_addr, process_name, min(first_seen), max(last_se
 from main.process_net_conn
 where remote_ip_addr='172.31.36.31'
 group by ALL order by all
-
+;
 
 -- All user activity
 SELECT hostname, user_name, min(first_seen), max(last_seen), count(distinct process_name), count(*)
@@ -27,7 +27,7 @@ from process
 where user_name like 'ACME%'
 group by ALL 
 order by all
-
+;
 
 -- Bad user activity
 SELECT hostname, user_name, min(first_seen), max(last_seen), count(distinct process_name), count(*)
@@ -35,6 +35,7 @@ from process
 where user_name like 'ACME\bad%'
 group by ALL 
 order by all
+;
 
 -- Benignware Processes
 SELECT hostname, process_name, min(first_seen), max(last_seen), count(distinct user_name), count(*)
@@ -42,12 +43,14 @@ from process
 where process_name like 'splunk%' or process_name like 'putty%'
 group by ALL 
 order by all
+;
 
 -- Event logs? Yes!
 select filename, count(distinct hostname), count(*) from main.process_file 
 where filename like '%evtx'
 group by all
 order by count(*) desc
+;
 
 
 
